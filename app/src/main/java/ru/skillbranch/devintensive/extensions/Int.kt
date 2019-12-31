9913dbf8d8d3bebbc4561665c0903d2446f375ba
Roman <roman161056@gmail.com>
2019-12-31 23:43:44 +0300

    Remove unused classes

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Int.kt
+++
@@ -1,8 +0,0 @@
-package ru.skillbranch.devintensive.extensions
-
-import android.content.res.Resources
-
-fun Int.dp(): Int = (this * Resources.getSystem().displayMetrics.density).toInt()
-
-fun Int.sp(): Int = (this * Resources.getSystem().displayMetrics.scaledDensity).toInt()
-
